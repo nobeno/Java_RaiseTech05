02c1d18bea75168c87c15f9437398e49c746389e
daichi nb <nd940501@gmail.com>
2019-10-09 20:53:55 +0900

    検索機能、計算機能追加

--- RaiseTech05/src/main/java/com/dayapp/controller/BusinessDaysController.java
+++ RaiseTech05/src/main/java/com/dayapp/controller/BusinessDaysController.java
@@ -1,5 +1,6 @@
 package com.dayapp.controller;
 
+import java.util.Calendar;
 import java.util.List;
 
 import org.mybatis.spring.annotation.MapperScan;
@@ -37,6 +38,51 @@ public class BusinessDaysController {
 	    mv.setViewName("app/index");
 	    List<BusinessDays> list = businessDaysMapper.selectByIdDays(keyword);
 	    mv.addObject("businessdays", list);
+	    return mv;
+	  }
+
+	@RequestMapping(value = "/calculation*", method = RequestMethod.POST)
+	  public ModelAndView calculation(
+			  @RequestParam Integer inputYear,
+			  @RequestParam Integer inputMonth,
+			  @RequestParam Integer inputDay,
+			  @RequestParam Integer addMonth,
+			  @RequestParam Integer selectDay
+			  ){
+	    ModelAndView mv = new ModelAndView();
+	    mv.setViewName("app/index");
+
+	    int year = inputYear;
+	    int month = inputMonth + addMonth;
+
+	    Calendar calendar = Calendar.getInstance();
+	    calendar.set(year,month-1,inputDay);
+
+	    mv.addObject("year",year);
+	    mv.addObject("month",month);
+
+	    String[] week = new String[7];
+	    	week[0]= "日";
+	    	week[1]= "月";
+	    	week[2]= "火";
+	    	week[3]= "水";
+	    	week[4]= "木";
+	    	week[5]= "金";
+	    	week[6]= "土";
+
+	    if(selectDay == 0) {
+	    	int Day = calendar.getActualMaximum(Calendar.DATE);
+	    	calendar.set(year,month-1,Day);
+	    	int week_int = calendar.get(Calendar.DAY_OF_WEEK);
+	    	mv.addObject("Day",Day);
+	    	mv.addObject("week",week[week_int-1]);
+	    }else {
+	    	int Day = selectDay;
+	    	calendar.set(year,month-1,Day);
+	    	int week_int = calendar.get(Calendar.DAY_OF_WEEK);
+	    	mv.addObject("Day",Day);
+	    	mv.addObject("week",week[week_int-1]);
+	    }
 	    return mv;
 	  }
 }
