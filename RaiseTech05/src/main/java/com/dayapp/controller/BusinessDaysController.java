602d6bc93a1c36a92a07ef27e64bb3d20f574c27
daichi nb <nd940501@gmail.com>
2019-10-13 21:50:18 +0900

    新規登録、編集、削除機能作成

--- RaiseTech05/src/main/java/com/dayapp/controller/BusinessDaysController.java
+++ RaiseTech05/src/main/java/com/dayapp/controller/BusinessDaysController.java
@@ -7,7 +7,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -85,4 +90,36 @@ public class BusinessDaysController {
 	    }
 	    return mv;
 	  }
+
+	@GetMapping("new")
+	public String BusinessDays(Model model) {
+		return "app/new";
+	}
+
+	@PostMapping
+	public String create(@ModelAttribute BusinessDays businessdays) {
+		businessDaysMapper.insertSelective(businessdays);
+		return "redirect:/app";
+	}
+
+	@GetMapping("{id}/edit")
+	public String edit(@PathVariable int id, Model model) {
+		BusinessDays businessday = businessDaysMapper.selectByPrimaryKey(id);
+		model.addAttribute("businessday", businessday);
+		return "app/edit";
+	}
+
+	@PutMapping("{id}")
+	public String update(@PathVariable int id, @ModelAttribute BusinessDays businessdays){
+		businessdays.setId(id);
+		businessDaysMapper.updateByPrimaryKeySelective(businessdays);
+		return "redirect:/app";
+	}
+
+	@DeleteMapping("{id}")
+	public String delete(@PathVariable int id) {
+		businessDaysMapper.deleteByPrimaryKey(id);
+		return "redirect:/app";
+	}
+
 }
